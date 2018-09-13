615c1a9a918d5aba6e88aad3ca13795a89ff8149
Ravi Srinivasan <rsriniva@redhat.com>
2018-09-13 11:16:40 +0530

    Fixes for Chap 4 second GE. moved DS to H2 instead of MySQl

---
+++ solutions/create-queries/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO Person (id,name) VALUES (1,'John');
+INSERT INTO Person (id,name) VALUES (2,'Steve');
+INSERT INTO Person (id,name) VALUES (3,'Mark');
+INSERT INTO Person (id,name) VALUES (4,'Nick');
+INSERT INTO Person (id,name) VALUES (5,'Nita');
+INSERT INTO Person (id,name) VALUES (6,'John');
+INSERT INTO Person (id,name) VALUES (7,'Julia');
+INSERT INTO Person (id,name) VALUES (8,'Joe');
+INSERT INTO Person (id,name) VALUES (9,'Nita');
+INSERT INTO Person (id,name) VALUES (10,'Branden');
+
