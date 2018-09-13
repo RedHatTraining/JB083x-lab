c7b9c849bf5207656f5694132ec63c5194b34fff
Ravi Srinivasan <rsriniva@redhat.com>
2018-09-13 17:46:45 +0530

    Migrate to H2 DB from MySQL for Chap 5 final GE

---
+++ labs/entity-relationships/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO Email VALUES (1,'development@example.com'),(2,'admin@example.com'),(3,'sales@example.com'),(4,'research@example.com'),(5,'ceo@example.com'),(6,'problems@example.com'),(7,'help-desk@example.com'),(8,'hr@example.com'),(9,'supplies@example.com');
+
+INSERT INTO UserGroup VALUES (1,'dev'),(2,'qa'),(3,'stage');                         
+
+INSERT INTO User (id,name,emailID,groupID) VALUES (1,'William',1,1),(2,'Rose',2,1),(3,'Pat',3,1),(4,'Rodney',4,2),(5,'Kim',5,2),(6,'Tom',6,2),(7,'Matt',7,3),(8,'George',8,3),(9,'Jean',9,3);
